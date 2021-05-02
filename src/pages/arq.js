e2147e31905e1ac50fa70d990eaccfc582b68f39
Lucas <lucas.chicoski@outlook.com>
2021-05-02 03:28:29 -0300

    new features 0.0.0.1

--- src/pages/arq.js
+++ src/pages/arq.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios'
 import logoPrincipal from '../images/newLogo2Borda.png';
 import nomeLogo from '../images/logoNameBorda.png';
 import gifRubiksCube from '../images/rubiks_cube.gif';
@@ -13,29 +14,69 @@ import './../CSS/footer.css'
 
 
 
-function Arq() {
+function Arq({ onSubmit }) {
+
+    const [senha, setSenha] = React.useState('');
+    const [email, setEmail] = React.useState('');
+
+
+    useEffect(() => {
+
+    }, []) //OBS. Caso o array estiver vazio, será executado uma vez
+
+    async function handleSubmit(e) {
+
+        console.log(senha + email)
+        //alert(senha + "/" + email )
+
+
+        axios.post('http://localhost:3333/login',{
+
+            email:email,
+            senha:senha
+
+        }).then(response => {
+            console.log(response)
+        }).catch(error => {
+            console.log(error.response)
+        });
+    }
+
 
     return (
         <div id="App">
             <header>
 
                 <div className="logoGeral">
-                    <a href="#">
-                    <img alt="teste" className='logo-principal' src={logoPrincipal} />
-                    <img alt="teste" className='nome-principal' src={nomeLogo} />
+                    <a href='!#'>
+                        <img alt="teste" className='logo-principal' src={logoPrincipal} />
+                        <img alt="teste" className='nome-principal' src={nomeLogo} />
                     </a>
                 </div>
 
                 <div className='Login'>
-                    <div >
-                        <input type="text" className='emaiL' title="teste" />
-                        <input type="text" className='senhA' />
-                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div >
+                            <input
+                                type="text"
+                                className='emaiL'
+                                title="teste"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)} />
 
-                    <div className="inputButton">
-                        <button className="botaoEntrar" type="submit">Entrar</button>
-                        <button className="botaoCadastrar" type="submit">Cadastrar</button>
-                    </div>
+                            <input
+                                type="text"
+                                className='senhA'
+                                value={senha}
+                                onChange={e => setSenha(e.target.value)} />
+                        </div>
+
+
+                        <div className="inputButton">
+                            <button className="botaoEntrar" type="submit">Entrar</button>
+                            <button className="botaoCadastrar" type="button">Cadastrar</button>
+                        </div>
+                    </form>
                 </div>
 
             </header>
@@ -47,7 +88,7 @@ function Arq() {
                         <img alt="teste" src={gifRubiksCube} />
                         <p>
                             Use seus conhecimentos para resolver problemas !
-                            </p>
+                        </p>
                     </div>
                 </div>
 
@@ -58,7 +99,7 @@ function Arq() {
                 </div>
 
                 <div className="inf3">
-                    <img src={GroupShare} />
+                    <img alt="teste" src={GroupShare} />
                     <p>" Crie grupos, compartilhe ideias e ajude projetos que possa muda a vida das pessoas"</p>
 
                 </div>
@@ -69,27 +110,27 @@ function Arq() {
                 <div className="GlobalFoot">
                     <div className="esquerda">
                         <li>
-                            <ul><a href="#">Quem Somos</a></ul>
-                            <ul><a href="#">Proposta da Turing Code</a></ul>
-                            <ul><a href="#">Ajude a Implementar</a></ul>
+                            <ul><a href='!#'>Quem Somos</a></ul>
+                            <ul><a href='!#'>Proposta da Turing Code</a></ul>
+                            <ul><a href='!#'>Ajude a Implementar</a></ul>
 
                         </li>
                     </div>
 
                     <div className="centro">
                         <li>
-                            <ul><a href="#">Desenvolvedores</a></ul>
-                            <ul><a href="#">Tecnologias envolvidas</a></ul>
-                            <ul><a href="#">Entre em contato</a></ul>
-                          
+                            <ul><a href='!#'>Desenvolvedores</a></ul>
+                            <ul><a href='!#'>Tecnologias envolvidas</a></ul>
+                            <ul><a href='!#'>Entre em contato</a></ul>
+
                         </li>
                     </div>
 
                     <div className="direita">
                         <li>
-                            <ul><a href="#">Apoie!</a></ul>
-                            <ul><a href="#">Seja uma empresa parceira</a></ul>
-                            <ul><a href="#">Box Tools preview</a></ul>
+                            <ul><a href='!#'>Apoie!</a></ul>
+                            <ul><a href='!#'>Seja uma empresa parceira</a></ul>
+                            <ul><a href='!#'>Box Tools preview</a></ul>
                         </li>
                     </div>
 
