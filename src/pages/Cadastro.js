0d8f6f9a5cedf54fe23a1fa52cbc013cc3099b27
Lucas <lucas.chicoski@outlook.com>
2021-05-25 22:55:30 -0300

    tela de cadastro v2

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -4,30 +4,33 @@ import '../CSS/Cadastro.css'
 
 import logo from './../images/newLogo2Borda.png'
 import logoName from './../images/logoNameBorda.png'
+import { Link } from 'react-router-dom';
 
 function Cadastro() {
     return (
-        <div className="mainframe">
-            <div className="cabecalho">
-                <div>
-                    <img className="logo" src={logo} />
+        <div className="tela">
+            <div className="mainframe">
+                <div className="cabecalho">
+                    <div>
+                      <Link to="/">  <img className="logo" src={logo} /></Link>
+                    </div>
+                    <div>
+                        <img className="nome" src={logoName} />
+                    </div>
                 </div>
-                <div>
-                    <img className="nome" src={logoName} />
-                </div>
-            </div>
 
-            <div className="formCadastro">
-                <div className="child">
-                    <p>Nome</p>
-                    <input />
-                    <p>Sobrenome</p>
-                    <input />
-                    <p>Senha</p>
-                    <input />
-                    <p>Interesse de trabalho</p>
-                    <input />
-                    <button type="button">Enviar</button>
+                <div className="formCadastro">
+                    <div className="child">
+                        <p>Nome</p>
+                        <input />
+                        <p>Sobrenome</p>
+                        <input />
+                        <p>Senha</p>
+                        <input />
+                        <p>Interesse de trabalho</p>
+                        <input />
+                        <button type="button">Cadastrar</button>
+                    </div>
                 </div>
             </div>
         </div>
